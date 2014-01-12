33ae395902b88400ebd751c208cd9e2d0dec1697
jineq <e115765@iu.u-ryukyu.ac.jp>
2014-01-12 19:27:43 +0900

    Removed warining

--- src/vending_machine/VendinMachine.java
+++ src/vending_machine/VendinMachine.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 
